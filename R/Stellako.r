4c58b93986ad2963893350a40e69bd2b890540bd
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2018-12-04 21:55:53 -0800

    Added Stellko.r and roxygen documentation

---
+++ R/Stellako.r
@@ -0,0 +1,15 @@
+#' Stellako data
+#'
+#' Data from on Effective Total Spawners,  ETS, and recruitment, Rec for
+#' one sockeye salmon stock, Stellako
+#'
+#' @docType data
+#'
+#' @usage data(Stellako)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+#' @source Sue Grant, DFO
+#'
