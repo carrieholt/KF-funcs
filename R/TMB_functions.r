0d6242cee789aa07136832983105daba22073525
Catarina Wor <Catarina Wor>
2022-05-09 18:04:40 -0700

    fix compiled cpps

--- R/TMB_functions.r
+++ R/TMB_functions.r
@@ -118,8 +118,8 @@ get_convergence_diagnostics <- function(sd_report) {
 #' This is a dummy function who's purpose is to hold the useDynLib roxygen tag.
 #' This tag will populate the namespace with compiled c++ functions upon package install.
 #'
-#' @useDynLib Rickerkf
 #' @useDynLib Ricker_rb_ratiovar
+#' @useDynLib Rickerkf
 #'
 dummy <- function(){
   return(NULL)
